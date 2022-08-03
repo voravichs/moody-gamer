79b766bf90b5b7462882a922b82fbb4dafd428da
VoravichS <vsilapac@gmail.com>
2022-08-03 13:07:18 -0500

    responsive pages up to the signs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,37 +9,8 @@ var horoscopeEl = $('#horoscope');
 var inputMoodEl = $('#user-input-mood');
 var horoscopeMoodEl = $('#horoscope-mood');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // make var for RAWG API
 var rawgAPIKey = '55468ae1e1444c17bf3c3a29d8b79732';
-
 
 // Handle changing page through carosel/cycling
 // display/showing
@@ -58,31 +29,32 @@ $(document).ready(function() {
         resultPageEl.toggleClass('hidden');
     });
 });
+
 // function to fetch data from RAWG
-getVideoGames = function(event) {
-    event.preventDefault();
-    var game = $('game-recs');
-    console.log(game);
+//getVideoGames = function(event) {
+//    event.preventDefault();
+//    var game = $('game-recs');
+//    console.log(game);
+//
+//    var rawgApiUrl = 'https://api.rawg.io/api/platforms?key=' + rawgAPIKey;
+//
+//    fetch (rawgApiUrl)
+//    .then (function (reponse) {
+//        return reponse.json()
+//    })
+//    .then (function (data) {
+//        console.log(data);
+//        document.querySelector('#game-recs').textContent = 
+//        document.querySelector('#game-genre').textContent = 
+//    })
+//}
 
-    var rawgApiUrl = 'https://api.rawg.io/api/platforms?key=' + rawgAPIKey;
-
-    fetch (rawgApiUrl)
-    .then (function (reponse) {
-        return reponse.json()
-    })
-    .then (function (data) {
-        console.log(data);
-        document.querySelector('#game-recs').textContent = 
-        document.querySelector('#game-genre').textContent = 
-    })
-}
-
-getHoroscope = function(event) {
-    event.preventDefault();
-    console.log(horoscopeMoodEl);
-
-    var aztroApiUrl = 
-}
+//getHoroscope = function(event) {
+//    event.preventDefault();
+//    console.log(horoscopeMoodEl);
+//
+//    var aztroApiUrl = 
+//}
 
 // function to fetch data from Aztro
 
