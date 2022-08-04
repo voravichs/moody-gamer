be5824c39c820baacf420e893501194f5b5c6a30
VoravichS <vsilapac@gmail.com>
2022-08-04 00:52:22 -0500

    adding style to add pages, delegation to img in signs page, replacing sign images with transparent ones

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var aztroAPIKey = '0c3a2c35c5mshca3267e96406afcp16bdbcjsn57d168946cdf';
 var startPageEl = $('#start-page');
 var moodPageEl = $('#mood-choices');
 var signPageEl = $('#sign');
-var resultPageEl = $('#result-page');
+var resultPageEl = $('#results-page');
 var gameGenreEl = $('game-genre');
 var gameRecsEl = $('#game-recs');
 var horoscopeEl = $('#horoscope');
@@ -15,20 +15,25 @@ var rawgAPIKey = '55468ae1e1444c17bf3c3a29d8b79732';
 // Handle changing page through carosel/cycling
 // display/showing
 $(document).ready(function() {
+    // Start page -> Sign Page
     $('#start-btn').click(function(){
         startPageEl.toggleClass('hidden');
-        moodPageEl.toggleClass('hidden');
+        signPageEl.toggleClass('hidden');
     });
+    // Sign page -> Mood Page
+    $('.btn').click(function(event){
+        var btnClicked = $(event.target);
+        if (btnClicked.is('img')) {
+            signPageEl.toggleClass('hidden');
+            moodPageEl.toggleClass('hidden');
+            console.log(event.target.alt);
+        }  
+    });
+    // Mood Page -> Results Page
     $('.icon').click(function(event){
         moodPageEl.toggleClass('hidden');
-        signPageEl.toggleClass('hidden');
-        console.log(event.target);
-    });
-    $('.btn').click(function(event){
-        // moodPageEl.toggleClass('hidden');
-        signPageEl.toggleClass('hidden');
         resultPageEl.toggleClass('hidden');
-        console.log(event.target.alt);
+        console.log(event.target.id);
     });
 });
 
@@ -40,8 +45,8 @@ fetch (rawgApiUrl)
 })
 .then (function (data) {
     console.log(data);
-    document.querySelector('#game-recs').textContent = data.name;
-    document.querySelector('#game-genre').textContent = data.genres;
+    $('#game-recs').textContent = data.name;
+    $('#game-genre').textContent = data.genres;
 })
 
 
